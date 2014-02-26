5edd5af008060fa8d6629693e6ed2e0b18e8e9ec
Paul Bennett <paul.w.bennett@gmail.com>
2014-02-26 03:21:01 -0500

    ugh

--- lib/MarpaX/SGML.pm
+++ lib/MarpaX/SGML.pm
@@ -17,20 +17,21 @@ our %EXPORT_TAGS = (
 BEGIN {
     push @INC, sub {
         my ($self, $filename) = @_;
+        my $package = $filename;
+        $package =~ s{/}{::}iog; # FIXME not platform-independent
+        $package =~ s/\.pm$//io;
+        return unless $package =~ /^MarpaX::SGML::Grammar::/;
         my $ebnf = $filename;
         $ebnf =~ s/\.pm$/.ebnf/io;
-        $ebnf =~ s{.+/}{}g;
+        $ebnf =~ s{.+/}{}g; # FIXME not platform-independent
         my $location = io()->catfile($ebnf)->absolute->canonpath;
-        $location =~ s{lib}{ebnf};
-        my @ebnf = io($location)->chomp->slurp;
-        my $package = $filename;
-        $package =~ s{[/\\]}{::}iog;
-        $package =~ s/\.pm$//io;
+        $location =~ s{/lib/}{/ebnf/}; # FIXME not platform-independent
+        my @contents = io($location)->chomp->slurp;
         my @lines = (
             "package $package;",
             "sub ebnf {",
             "    return <<'MARPA';",
-            @ebnf,
+            @contents,
             "MARPA",
             "}",
             "1;",
@@ -46,8 +47,8 @@ BEGIN {
 {
     my $master_grammar = { };
     for my $i (qw( Abstract Axiomatic DTD DefaultG0 DefaultG1 LTD Prolog SGMLDeclaration SystemDeclaration )) {
-        eval('require MarpaX::SGML::' . $i) or die $@;
-        $master_grammar->{ $i } = eval('MarpaX::SGML::' . $i . '::ebnf()') or die $@;
+        eval('require MarpaX::SGML::Grammar::' . $i) or die $@;
+        $master_grammar->{ $i } = eval('MarpaX::SGML::Grammar::' . $i . '::ebnf()') or die $@;
     }
     sub master_grammar {
         return join("\n", values %$master_grammar);
@@ -107,153 +108,35 @@ sub xmlPI () { return (':TOP' => \&_ensure_xml_pi); }
 sub new {
     my $class = shift;
     $class = ref($class) || $class;
-    my $self = bless { }, $class;
+    my %args = ref($_[0]) ? %{$_[0]} : @_;
+    my $self = bless {
+        G_args => { },
+        R_args => { },
+        %args,
+    }, $class;
     $self->clean_grammar();
     return $self;
 }
 
-sub add_system {
-    my $self = shift;
-    my ($sysname, $sysdoc) = @_;
-    $self->{ system } ||= { };
-    $self->{ system_order } ||= [ ];
-    if (!$self->{ system }->{ $sysname }) {
-        if (!$sysdoc) {
-            my $class
-                = eval("require $sysname") ? $sysname
-                : eval("require MarpaX::SGML::System::${sysname}") ? "MarpaX::SGML::System::${sysname}"
-                : croak("No such System")
-                ;
-            $sysdoc
-                = $class->can('new') ? $class->new($self)->system_document()
-                : $class->system_document()
-                ;
-        }
-        $self->{ system }->{ $sysname } = $sysdoc;
-        push @{$self->{ system_order }}, $sysname;
-    }
-    return $self;
-}
-
-sub remove_system {
-    my $self = shift;
-    my ($sysname) = @_;
-    $self->{ system } ||= { };
-    $self->{ system_order } ||= [ ];
-    delete $self->{ system }->{ $sysname };
-    @{$self->{ system_order }} = grep { $_ ne $sysname } @{$self->{ system_order }};
-    return $self;
-}
-
-sub promote_system {
-    my $self = shift;
-    my ($sysname) = @_;
-    $self->{ system } ||= { };
-    $self->{ system_order } ||= [ ];
-    if (!$self->{ system }->{ $sysname }) {
-        croak("System `$sysname' is not (currently) part of this object");
-    }
-    @{$self->{ system_order }} = ($sysname, (grep { $_ ne $sysname } @{$self->{ system_order }}));
-    return $self;
-}
-
-sub demote_system {
-    my $self = shift;
-    my ($sysname) = @_;
-    $self->{ system } ||= { };
-    $self->{ system_order } ||= [ ];
-    if (!$self->{ system }->{ $sysname }) {
-        croak("System `$sysname' is not (currently) part of this object");
-    }
-    @{$self->{ system_order }} = ((grep { $_ ne $sysname } @{$self->{ system_order }}), $sysname);
-    return $self;
-}
-
-sub get_system {
-    my $self = shift;
-    $self->{ system } ||= { };
-    $self->{ system_order } ||= [ ];
-    return join('', map { $self->{ system }->{ $_ } . pack('C2', (10, 13)) } @{$self->{ system_order }});
-}
-
-sub parse {
-    my $class;
-    $class = shift if eval { $_[0]->isa(__PACKAGE__) } || $_[0] eq __PACKAGE__;
-    $class ||= __PACKAGE__;
-    my $self = ref($class) ? $class : $class->new();
-    my ($datathing, @rest) = @_;
-    @rest = map { $_ = ref($_) eq 'HASH' ? %$_ : $_ } @rest;
-    my $rawdata;
-    if (ref $datathing eq 'SCALAR') {
-        $rawdata = $$datathing;
-    }
-    elsif (my $source = eval { io($rawdata) }) {
-        $rawdata = $source->slurp();
-    }
-    $self->{ commands } = [ @rest ];
-    $self->parser()->{ controller } = $self;
-    $self->parser()->read($self->get_system() . $rawdata);
-    return $self->parser()->value();
-}
-
 sub sgml {
-    my $class;
-    $class = shift if eval { $_[0]->isa(__PACKAGE__) } || $_[0] eq __PACKAGE__;
-    $class ||= __PACKAGE__;
-    my $self = ref($class) ? $class : $class->new();
-    my $want_ref = !!ref($_[0]);
-    my $ast = $self->parse(@_);
-    return $want_ref ? \($ast->toString()) : $ast->toString();
+    my $self = eval { $_[0]->isa(__PACKAGE__) } ? shift(@_) : __PACKAGE__->new();
+    my $document = \do{ shift(@_) };
+    my %args = ref($_[0]) ? %{$_[0]} : @_;
+    my $G = Marpa::R2::SLIF::G->new({ %{$self->{ G_args }}, source => \do{ $self->{ ebnf }} });
+    my $R = Marpa::R2::SLIF::R->new({ %{$self->{ R_args }}, grammar => $G });
+    $R->read($document);
+    my $scoreboard = bless \%args, 'MarpaX::SGML::Semantics';
+    my $AST = $R->value($scoreboard) or die 'No Parse';
+    $AST->walk_down({ callback => sub { $_[1]->{ scoreboard } ||= $scoreboard; _stringify_node(@_) }, });
+    my $rv = $scoreboard->{ output };
+    $AST->delete_tree();
+    return $rv;
 }
 
-sub libxml {
-    my $class;
-    $class = shift if eval { $_[0]->isa(__PACKAGE__) } || $_[0] eq __PACKAGE__;
-    $class ||= __PACKAGE__;
-    my $self = ref($class) ? $class : $class->new();
-    return $self->parse(@_, fullyTagged, integrallyStored, xmlPI)->toLibXML();
-}
-
-sub ebnf {
-    my $path = io->catfile(split(/::/, __PACKAGE__))->relative()->filename() . '.pm';
-    my $location = io->catfile($INC{ $path })->updir()->canonpath();
-    return \(io->catfile($location, 'SGML.ebnf')->slurp());
-}
-
-sub parser {
-    my $class;
-    $class = shift if eval { $_[0]->isa(__PACKAGE__) } || $_[0] eq __PACKAGE__;
-    $class ||= __PACKAGE__;
-    my $self = ref($class) ? $class : $class->new();
-    if (!$self->{ G }) {
-        $self->{ G } = Marpa::R2::Scanless::G->new({ bless_package => 'MarpaX::SGML::Actions', source => $self->ebnf() });
-    }
-    $self->{ R } ||= Marpa::R2::Scanless::R->new($self->{ G });
-    return $self->{ R };
-}
-
-sub _does_rule_apply {
-    my ($piece, $rule) = @_;
-    return 1 if $rule eq '*';
-    return _does_tag_apply($piece, $rule) unless $rule =~ /^([:]\w+)((?:\[).+?(?:\]))?$/;
-    my ($rtype, $rattr) = ($1, $2);
-    return unless substr($piece->{ isa }, -length($rtype)) eq $rtype;
-    $rattr =~ s/\@(\w+)\s*(?!:=)/exists \$piece->{'$1'}/g;
-    $rattr =~ s/\@(\w+)\s*=\s*(.+?)/\$piece->{'$1'} ~~ $2/g;
-    return eval($rattr);
-}
-
-sub _does_tag_apply {
-    my ($piece, $tag) = @_;
-    return 1 if $tag eq '*';
-    return unless $tag =~ /^([^:]\w+)((?:\[).+?(?:\]))?$/;
-    my ($ttype, $tattr) = ($1, $2);
-    return unless $piece->{ isa } =~ /:TAG$/;
-    return unless $piece->{ tagtype } eq $ttype;
-    return 1 unless $tattr;
-    $tattr =~ s/\@(\w+)\s*(?!:=)/exists \$piece->{'$1'}/g;
-    $tattr =~ s/\@(\w+)\s*=\s*(.+?)/\$piece->{'$1'} ~~ $2/g;
-    return eval($tattr);
+sub _stringify_node {
+    my ($node, $options) = @_;
+    $options->{ scoreboard }->{ output } .= ($node->name || $node);
+    return 1;
 }
 
 sub IsDataChar {
